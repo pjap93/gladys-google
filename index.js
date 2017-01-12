e75991b41138d776dea44bb52c4cc7f25d95f77c
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2017-01-12 13:50:46 +0100

    Synchronize calendars and events

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+module.exports = function (sails) {
+
+    var connect = require('./lib/connect.js');
+    var authenticate = require('./lib/authenticate.js');
+    var getRedirectUrl = require('./lib/getRedirectUrl.js');
+    var sync = require('./lib/calendar/calendar.sync.js');
+
+    return {
+        getRedirectUrl,
+        authenticate,
+        calendar: {
+            sync
+        }
+    };
+};
