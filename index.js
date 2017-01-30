37c1444d42477c480aa4d37b93ed471949556b30
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2017-01-30 21:20:38 +0100

    add setup function

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ module.exports = function (sails) {
     var authenticate = require('./lib/authenticate.js');
     var getRedirectUrl = require('./lib/getRedirectUrl.js');
     var sync = require('./lib/calendar/calendar.sync.js');
+    var setup = require('./lib/setup.js');
 
     return {
+        setup,
         getRedirectUrl,
         authenticate,
         calendar: {
